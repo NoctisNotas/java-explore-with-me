4c304ac980b3940c6f1bc5e4b8d96a7a9c48c8b3
Arsentij Buyakov <aobuyakov@gmail.com>
2026-01-12 15:33:49 +0300

    fix for postman tests

--- main-service/src/main/java/ru/practicum/explorewithme/dto/event/NewEventDto.java
+++ main-service/src/main/java/ru/practicum/explorewithme/dto/event/NewEventDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.explorewithme.dto.event;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -35,6 +36,8 @@ public class NewEventDto {
     private EventLocation location;
 
     private Boolean paid = false;
+
+    @Min(0)
     private Integer participantLimit = 0;
     private Boolean requestModeration = true;
 
