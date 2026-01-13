a95dd529b30b3bb4000881c64f1749e996d01ed1
Arsentij Buyakov <aobuyakov@gmail.com>
2026-01-13 16:13:34 +0300

    fix for postman tests

--- main-service/src/main/java/ru/practicum/explorewithme/controller/admin/AdminEventController.java
+++ main-service/src/main/java/ru/practicum/explorewithme/controller/admin/AdminEventController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class AdminEventController {
 
     private final EventService eventService;
--- main-service/src/main/java/ru/practicum/explorewithme/controller/open/OpenEventController.java
+++ main-service/src/main/java/ru/practicum/explorewithme/controller/open/OpenEventController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class OpenEventController {
 
     private final EventService eventService;
