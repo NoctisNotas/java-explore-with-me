57fefdba2ff15a1d3fcbaadeccbf8772325df9d0
Arsentij Buyakov <aobuyakov@gmail.com>
2026-01-13 13:04:46 +0300

    fix for postman tests

--- main-service/src/main/java/ru/practicum/explorewithme/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/explorewithme/repository/EventRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.explorewithme.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.explorewithme.model.Event;
 import ru.practicum.explorewithme.model.EventState;
 
@@ -32,22 +33,21 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("SELECT e FROM Event e " +
             "WHERE e.state = 'PUBLISHED' " +
-            "AND (:text IS NULL OR e.annotation LIKE CONCAT('%', :text, '%') " +
-            "OR e.description LIKE CONCAT('%', :text, '%')) " +
-            "AND (:categories IS NULL OR e.category.id IN :categories) " +
+            "AND (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
+            "     OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
+            "AND (:categories IS NULL OR :categories IS EMPTY OR e.category.id IN :categories) " +
             "AND (:paid IS NULL OR e.paid = :paid) " +
             "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
             "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) " +
-            "AND (:onlyAvailable IS NULL OR " +
-            "e.participantLimit = 0 OR " +
-            "e.confirmedRequests < e.participantLimit))")
+            "AND (:onlyAvailable IS NULL OR :onlyAvailable = false OR " +
+            "     (e.participantLimit = 0 OR e.confirmedRequests < e.participantLimit))")
     List<Event> findEventsByPublic(
-            String text,
-            List<Long> categories,
-            Boolean paid,
-            LocalDateTime rangeStart,
-            LocalDateTime rangeEnd,
-            Boolean onlyAvailable,
+            @Param("text") String text,
+            @Param("categories") List<Long> categories,
+            @Param("paid") Boolean paid,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
+            @Param("onlyAvailable") Boolean onlyAvailable,
             Pageable pageable);
 
     Long countByCategoryId(Long categoryId);
